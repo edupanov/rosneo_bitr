d03610a2de4d61ff114232d1bc6344afcf7858f4
Egor <edupanov@gmail.com>
2021-12-21 16:02:44 +0300

    add our company

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-const projectImg = document.querySelector('.project_img')
-const buildImg = document.querySelector('.build_img')
-const exploitationImg = document.querySelector('.exploitation_img')
-const project = document.querySelector('.project')
-const build = document.querySelector('.build')
-const exploitation = document.querySelector('.exploitation')
-const sliderItem = document.querySelector('.slider_item')
-const sliderTitle = document.querySelector('.slider_title')
-let slider = document.querySelector('.main_slider')
+
+const slider = document.querySelector('.main_slider')
+const projectChecked = document.querySelector('.project_checked')
 
 const sliderData = {
     project: {
@@ -46,62 +40,64 @@ const sliderData = {
 }
 
 slider.addEventListener('click', (event) => {
-    console.log(event.target.className)
 
     switch (event.target.className) {
         case 'project_img':
             slider.innerHTML = `
-            <div class="slider_item project">
+            <div class="slider_item">
                 <h1 class='slider_main_title'>${sliderData.project.title}</h1>
-                <img class='${sliderData.project.changeClassNameFull}' src='${sliderData.project.changeSrcFull}' alt="">
+                <img class='${sliderData.project.changeClassNameFull}' src='${sliderData.project.changeSrcFull}' alt="project">
                 <div class="change_text">${sliderData.project.text}</div>
                 <button class="change_button">${sliderData.project.button}</button>\
             </div>
-            <div class="slider_item  build">
+            <div class="slider_item">
                 <h2 class="slider_title-rotate">${sliderData.build.title}</h2>
-                <img class='build_img' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.build.changeSrcMin}' alt="">
+                <img class='${sliderData.build.className}' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.build.changeSrcMin}' alt="build">
             </div>
-            <div class="slider_item exploitation">
+            <div class="slider_item">
                 <h2 class="slider_title-rotate">${sliderData.exploitation.title}</h2>
-                <img class='exploitation_img' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.exploitation.changeSrcMin}' alt="">
+                <img class='${sliderData.exploitation.className}' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.exploitation.changeSrcMin}' alt="exploitation">
             </div>
     `;
+            projectChecked.style.display = 'block'
             break;
         case 'build_img':
             slider.innerHTML = `
-            <div class="slider_item  project">
+            <div class="slider_item">
                 <h2 class="slider_title-rotate">${sliderData.project.title}</h2>
-                <img class='project_img' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.project.changeSrcMin}' alt="">
+                <img class='${sliderData.project.className}' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.project.changeSrcMin}' alt="project">
             </div>
-            <div class="slider_item  build" >
+            <div class="slider_item" >
                 <h1 class='slider_main_title'>${sliderData.build.title}</h1>
-                <img class='${sliderData.build.changeClassNameFull}' src='${sliderData.build.changeSrcFull}' alt="">
+                <img class='${sliderData.build.changeClassNameFull}' src='${sliderData.build.changeSrcFull}' alt="build">
                 <div class="change_text">${sliderData.build.text}</div>
                 <button class="change_button">${sliderData.build.button}</button>\
             </div>
-            <div class="slider_item exploitation">
+            <div class="slider_item">
                 <h2 class="slider_title-rotate">${sliderData.exploitation.title}</h2>
-                <img class='exploitation_img' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.exploitation.changeSrcMin}' alt="">
+                <img class='${sliderData.exploitation.className}' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.exploitation.changeSrcMin}' alt="exploitation">
             </div>
     `;
+            projectChecked.style.display = 'none'
             break;
         case 'exploitation_img':
             slider.innerHTML = `
-            <div class="slider_item  project">
+            <div class="slider_item">
                 <h2 class="slider_title-rotate">${sliderData.project.title}</h2>
-                <img class='project_img' src='${sliderData.project.changeSrcMin}' style="max-width: 120px ; z-index: 1 ;" alt="">
+                <img class='${sliderData.project.className}' src='${sliderData.project.changeSrcMin}' style="max-width: 120px ; z-index: 1 ;" alt="project">
             </div>
-            <div class="slider_item  build">
+            <div class="slider_item">
                 <h2 class="slider_title-rotate">${sliderData.build.title}</h2>
-                <img class='build_img' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.build.changeSrcMin}' alt="">
+                <img class='${sliderData.build.className}' style="max-width: 120px ; z-index: 1 ;" src='${sliderData.build.changeSrcMin}' alt="build">
             </div>
-            <div class='slider_item exploitation'>
+            <div class='slider_item'>
                 <h1 class='slider_main_title'>${sliderData.exploitation.title}</h1>
-                <img class='${sliderData.exploitation.changeClassNameFull}' src='${sliderData.exploitation.changeSrcFull}' alt="">
+                <img class='${sliderData.exploitation.changeClassNameFull}' src='${sliderData.exploitation.changeSrcFull}' alt="exploitation">
                 <div class="change_text">${sliderData.exploitation.text}</div>
                 <button class="change_button">${sliderData.exploitation.button}</button>\
             </div>
     `;
+            projectChecked.style.display = 'none'
             break;
     }
 })
